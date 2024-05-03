c5c3115337ce0813741dc87ee8ff160d9f8973f0
Amit Eshore <amiteshore@gmail.com>
2024-05-03 13:47:58 +0530

    Add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://irregular-iron.netlify.app"
+});
