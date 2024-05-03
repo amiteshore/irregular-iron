bdc11099fa85561a264c4dbdcf2c4081015d199b
Amit Eshore <amiteshore@gmail.com>
2024-05-03 14:10:23 +0530

    Add Greeting with astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://irregular-iron.netlify.app"
+  site: "https://irregular-iron.netlify.app",
+  integrations: [preact()]
 });
